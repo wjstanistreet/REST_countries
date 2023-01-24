1620ba9fc06920e219f5a15729403a0b32fb0f34
Saad24 <saad.24.ahm@gmail.com>
2023-01-24 17:29:02 +0000

    Working list with selections shown

--- src/components/Country.js
+++ src/components/Country.js
@@ -1,17 +1,35 @@
+import CountryContainer from "../containers/CountryContainer";
 
-
-const Country = ({country}) => {
+const Country = ({countries}) => {
 
     const handleCountryForm = (event) => {
         event.preventDefault();
         // setSearch(event.target[0].value);
     };
 
+    const countryList = countries.map((country, id) => {
+        
+        return (
+        <form name="newform" type="text" key = {id}>
+        <li>{country.name.common}</li>
+
+            <input type="checkbox"></input>
+
+        </form>
+                
+        )
+
+    })
     return(
+        
+        <> 
             <form name="input_country" type="text" onSubmit={handleCountryForm} >
                 <input type="text" placeholder="Enter country"/>
                 <input type="submit" value="OK"/>
             </form>
+
+            <ul>Country to visit: {countryList}</ul> 
+        </>
     );
 };
 
--- src/containers/CountryContainer.js
+++ src/containers/CountryContainer.js
@@ -1,24 +1,23 @@
 import { useEffect, useState } from "react";
 import Country from "../components/Country";
 
-const CountryContainer = (event) => {
+const CountryContainer = () => {
 
-    const [country, setCountry] = useState(null);
+    const [countries, setCountries] = useState([]);
+    
 
     // const [search, setSearch] = useState("");
 
     // const search = event.target[0].value;
 
     useEffect(() => {
-        fetch(`https://restcountries.com/v3.1/name/Peru`)
+        fetch(`https://restcountries.com/v3.1/all`)
         .then(response => response.json())
-        .then(data => setCountry(data[0][0]))
-        console.log(country);
-    }, []);
+        .then(data => setCountries(data))}, []);
 
     return(
         <>
-            <Country country={country}/>
+            <Country countries={countries}/>
         </>
     );
 
